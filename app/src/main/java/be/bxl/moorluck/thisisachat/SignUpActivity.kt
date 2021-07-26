bc9aec6b63fbfd859ebb4a9067dafde1889184a8
William <wgenard94@gmail.com>
2021-07-26 12:14:46 +0200

    layout sign in

---
+++ app/src/main/java/be/bxl/moorluck/thisisachat/SignUpActivity.kt
@@ -0,0 +1,11 @@
+package be.bxl.moorluck.thisisachat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
