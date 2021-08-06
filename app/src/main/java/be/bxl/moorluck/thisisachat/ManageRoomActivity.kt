1255f45d8b081bac6cb0078d7e2a9fcf7cd02aa4
William <wgenard94@gmail.com>
2021-08-06 17:01:24 +0200

    initial manage room activiyt layout

--- app/src/main/java/be/bxl/moorluck/thisisachat/ManageRoomActivity.kt
+++ app/src/main/java/be/bxl/moorluck/thisisachat/ManageRoomActivity.kt
@@ -1,11 +1,18 @@
 package be.bxl.moorluck.thisisachat
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import be.bxl.moorluck.thisisachat.consts.FirebaseConst
 import be.bxl.moorluck.thisisachat.models.Room
+import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DatabaseReference
@@ -16,6 +23,10 @@ import com.google.firebase.storage.ktx.storage
 
 class ManageRoomActivity : AppCompatActivity() {
 
+    companion object {
+        const val LABEL_COPY = "LABEL_COPY"
+    }
+
     //Firebase
 
     lateinit var auth : FirebaseAuth
@@ -25,6 +36,11 @@ class ManageRoomActivity : AppCompatActivity() {
     // View
 
     lateinit var tvRoomId : TextView
+
+    lateinit var etName : EditText
+
+    lateinit var imgCopy : ImageView
+    lateinit var imgRoom : ImageView
 
     // Room info
 
@@ -48,6 +64,11 @@ class ManageRoomActivity : AppCompatActivity() {
 
         tvRoomId = findViewById(R.id.tv_room_id_manage_room_activity)
 
+        etName = findViewById(R.id.et_name_manage_room_activity)
+
+        imgCopy = findViewById(R.id.img_copy_id_manage_room_activity)
+        imgRoom = findViewById(R.id.img_room_manage_room_activity)
+
         // Setup the action bar
 
         supportActionBar?.setDisplayShowHomeEnabled(true)
@@ -57,7 +78,16 @@ class ManageRoomActivity : AppCompatActivity() {
         val roomType = intent.getStringExtra(ChatActivity.ROOM_TYPE)
         if (roomId != null && roomType != null) {
             getRoomInfo(roomId, roomType)
-            tvRoomId.text = roomId
+        }
+
+        // OnClick
+
+        imgCopy.setOnClickListener {
+            val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clip = ClipData.newPlainText(LABEL_COPY, roomId)
+            clipboard.setPrimaryClip(clip)
+
+            Toast.makeText(this, "Room ID succesfully copied !", Toast.LENGTH_LONG).show()
         }
 
     }
@@ -68,9 +98,25 @@ class ManageRoomActivity : AppCompatActivity() {
             .addOnSuccessListener {
                 if (it.exists()) {
                     room = it.getValue(Room::class.java)!!
-                    supportActionBar?.title = room.name
+
+                    updateUI(room)
+
                 }
             }
+    }
+
+    private fun updateUI(room : Room) {
+        supportActionBar?.title = room.name
+
+        val text = tvRoomId.text.toString() + " " + room.id
+        tvRoomId.text = text
+
+        etName.setText(room.name)
+
+        Glide.with(this)
+            .load(room.photoRef)
+            .into(imgRoom)
+
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
